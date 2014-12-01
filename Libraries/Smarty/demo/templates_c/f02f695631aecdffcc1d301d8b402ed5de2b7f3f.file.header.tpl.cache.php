25ce16b958a6799931040e53e57ecb8a15e6ca69
rummsi <rummsi@1dd36da9-faee-444c-9146-05a6d0fa0afe>
2014-12-01 14:25:45 +0000

    NEW - Carregamento do Smarty Template Engine.

---
+++ Libraries/Smarty/demo/templates_c/f02f695631aecdffcc1d301d8b402ed5de2b7f3f.file.header.tpl.cache.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2014-11-17 21:37:27
+         compiled from ".\templates\header.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:28469546a6a97b7cc30-08984284%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'f02f695631aecdffcc1d301d8b402ed5de2b7f3f' => 
+    array (
+      0 => '.\\templates\\header.tpl',
+      1 => 1413663676,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '28469546a6a97b7cc30-08984284',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'title' => 0,
+    'Name' => 1,
+  ),
+  'has_nocache_code' => true,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_546a6a97b887b7_15426224',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_546a6a97b887b7_15426224')) {function content_546a6a97b887b7_15426224($_smarty_tpl) {?><HTML>
+<HEAD>
+    <TITLE><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+ - <?php echo '/*%%SmartyNocache:28469546a6a97b7cc30-08984284%%*/<?php echo $_smarty_tpl->tpl_vars[\'Name\']->value;?>
+/*/%%SmartyNocache:28469546a6a97b7cc30-08984284%%*/';?>
+</TITLE>
+</HEAD>
+<BODY bgcolor="#ffffff">
+<?php }} ?>
