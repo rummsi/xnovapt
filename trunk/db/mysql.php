77f08d4430413d5a4bc24bc76c05c7a417909805
rummsi <rummsi@1dd36da9-faee-444c-9146-05a6d0fa0afe>
2015-05-19 20:26:30 +0000

    - criada a função mysqli_result.

--- trunk/db/mysql.php
+++ trunk/db/mysql.php
@@ -63,3 +63,9 @@ function doquery($query, $table, $fetch = false)
         return $sqlQuery;
     }
 }
+
+function mysqli_result($res, $row, $field=0) { 
+    $res->data_seek($row); 
+    $datarow = $res->fetch_array(); 
+    return $datarow[$field]; 
+}
