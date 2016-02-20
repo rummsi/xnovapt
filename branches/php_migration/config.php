1df433a2d5687730a83cfca26ae2c79e44e5a32e
rummsi <rummsi@1dd36da9-faee-444c-9146-05a6d0fa0afe>
2016-02-20 10:24:01 +0000

    - Instalada a aplicação na base de dados. - Projecto netbeans.

---
+++ branches/php_migration/config.php
@@ -0,0 +1,14 @@
+<?php return array(
+    'global' => array(
+        'database' => array(
+            'engine' => 'mysql',
+            'options' => array(
+                'hostname' => 'localhost',
+                'username' => 'ptgamespt',
+                'password' => 'ptgamespt',
+                'database' => 'ptgamespt'
+                ),
+            'table_prefix' => 'oficialfix_',
+            )
+        )
+    );
